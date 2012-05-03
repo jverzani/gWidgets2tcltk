0451a73722e9bc3b4e98faadb269ab20c91c1d48
John Verzani <jverzani@gmail.com>
2012-05-03 13:00:50 -0400

    add set_value method

--- R/gformlayout.R
+++ R/gformlayout.R
@@ -78,6 +78,15 @@ GFormLayout <- setRefClass("GFormLayout",
                                names(children) <<- c(nms, label)
                              },
                              get_value=function(...) sapply(children, svalue, simplify=FALSE),
+                             set_value=function(value, ...) {
+                               "value a named list matching children"
+                               value <- as.list(value)
+                               nms <- Filter(function(i) !is.na(match(i, names(children))), names(value))
+                               sapply(nms, function(nm) {
+                                 obj <- children[[nm]]
+                                 svalue(obj) <- value[[nm]]
+                               })
+                             },
                              get_dim=function() {
                                d <- rev(as.numeric(tcl("grid","size", widget)))
                                setNames(d, c("nrow", "ncol"))
